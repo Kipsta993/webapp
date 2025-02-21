ff0faff9d133c566b83960097d34d068222b87a7
Kipsta993 <154733059+Kipsta993@users.noreply.github.com>
2025-02-21 15:16:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ document.querySelectorAll('.complete-btn').forEach((button, index) => {
     const isCompleted = localStorage.getItem(`task_${index}_completed`) === 'true';
     if (isCompleted) {
         button.classList.add('completed');
+        button.closest('.task-card').classList.add('completed');
     }
 
     button.addEventListener('click', function() {
         if (!this.classList.contains('completed')) {
             this.classList.add('completed');
+            this.closest('.task-card').classList.add('completed');
             // Сохраняем состояние в localStorage
             localStorage.setItem(`task_${index}_completed`, 'true');
         }
